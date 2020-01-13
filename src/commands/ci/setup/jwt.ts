54698ad25282858e8fda8c165fbb081ee2e99256
Sébastien Colladon <colladonsebastien@gmail.com>
2020-01-13 20:08:03 +0100

    :sparkles: use execSync and the right command line
    as in the file bootstrap-jwt-auth-prerequisites.sh of the repo https://github.com/mehdisfdc/sfdx-BitbucketPipeline
    fixes issue when trying to decrypt encrypted file

--- src/commands/ci/setup/jwt.ts
+++ src/commands/ci/setup/jwt.ts
@@ -2,7 +2,7 @@ import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
 import * as fs from 'fs';
-import * as openssl from 'openssl-nodejs';
+import { execSync } from 'child_process';
 import * as fse from 'fs-extra';
 import * as path from 'path';
 
@@ -23,41 +23,9 @@ const CSR_OPTIONS = {
   }
 };
 
+const tmpFolder = 'tmp';
+
 export default class Jwt extends SfdxCommand {
-
-  /* Use another lib openssl-nodejs and fs-extra to create the temporary folder and files required to the generation of the certificat
-COUNTRY_NAME="FR"
-STATE="France"
-LOCALITY="Paris"
-ORGANIZATION_NAME="sfdx"
-ORGANIZATIONAL_UNIT="jwt:auth"
-COMMON_NAME="jwt.auth.com"
-EMAIL="sfdx@jwt.auth"
-CERTIFICATE_EXPIRE_DAYS=365
-
-#### CREATE CERTIFICATE AND PRIVATE KEY ############################################################
-#
-mkdir ./tmp 2>/dev/null
-cd tmp
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048 2>/dev/null
-openssl rsa -passin pass:x -in server.pass.key -out server.key 2>/dev/null
-openssl req -new -key server.key -out server.csr \
-            -subj "/C=$COUNTRY_NAME/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION_NAME/OU=$ORGANIZATIONAL_UNIT/CN=$COMMON_NAME/emailAddress=$EMAIL" 2>/dev/null
-
-openssl x509 -req -sha256 -days $CERTIFICATE_EXPIRE_DAYS -in server.csr -signkey server.key -out "$ENV.crt" 2>/dev/null
-
-
-mkdir ../build 2>/dev/null
-mkdir ../certificate 2>/dev/null
-
-openssl aes-256-cbc -salt -e -in server.key -out "${ENV}_server.key.enc" -pass pass:$PASSWORD 2>/dev/null
-mv "${ENV}_server.key.enc" ../build
-mv "$ENV.crt" ../certificate
-cd ..
-rm -rf tmp
-echo "$ENV.crt in the build folder"
-echo "${ENV}_server.key.enc created in the certificate folder"
-  */
 
   public static description = messages.getMessage('command');
 
@@ -76,21 +44,19 @@ echo "${ENV}_server.key.enc created in the certificate folder"
       this.ux.log(messages.getMessage('folderDoNotExist', [this.flags.output]));
       return null;
     }
+    fse.ensureDirSync(tmpFolder);
     try {
-      const tmpDir = 'tmp';
-      fse.mkdirSync(tmpDir);
-      openssl(`genrsa -des3 -passout pass:x -out ${tmpDir}/server.pass.key 2048`);
-      openssl(`rsa -passin pass:x -in ${tmpDir}/server.pass.key -out ${tmpDir}/server.key`);
-      openssl(`req -new -key ${tmpDir}/server.key -out ${tmpDir}/server.csr -subj "/C=FR/ST=FRANCE/L=PARIS/O=sfdx/OU=sfdx:auth:jwt/CN=jwt/emailAddress=sfdx@jwt.auth`);
-      openssl(`x509 -req -sha256 -days 365 -in ${tmpDir}/server.csr -signkey ${tmpDir}/server.key -out "${this.flags.output}/${this.flags.env}.crt"`);
-      openssl(`aes-256-cbc -salt -e -in ${tmpDir}/server.key -out "${this.flags.output}/${ENV}_server.key.enc" -pass pass:${this.flags.password}`);
-      fse.rmdirSync(tmpDir);
-
-      this.ux.log(messages.getMessage('successCopy', [`${ENV}_server.key.enc`, path.resolve(this.flags.output)]));
-      this.ux.log(messages.getMessage('successCopy', [`${this.flags.env}.crt`, path.resolve(this.flags.output)]));
+      execSync(`openssl genrsa -des3 -passout pass:x -out ${path.join(tmpFolder, 'server.pass.key')} 2048 2>/dev/null`)
+      execSync(`openssl rsa -passin pass:x -in ${path.join(tmpFolder, 'server.pass.key')} -out ${path.join(tmpFolder, 'server.key')}  2>/dev/null`)
+      execSync(`openssl req -new -key ${path.join(tmpFolder, 'server.key')} -out ${path.join(tmpFolder, 'server.csr')} -subj "/C=FR/ST=FRANCE/L=PARIS/O=sfdx/OU=sfdx:auth:jwt/CN=jwt/emailAddress=sfdx@jwt.auth"  2>/dev/null`)
+      execSync(`openssl x509 -req -sha256 -days 365 -in ${path.join(tmpFolder, 'server.csr')} -signkey ${path.join(tmpFolder, 'server.key')} -out ${path.join(this.flags.output, this.flags.env + '.crt')}  2>/dev/null`)
+      execSync(`openssl aes-256-cbc -salt -e -in ${path.join(tmpFolder, 'server.key')} -out ${path.join(this.flags.output, this.flags.env + '_server.key.enc')} -pass pass:${this.flags.password}  2>/dev/null`)
     } catch (ex) {
       throw new SfdxError(messages.getMessage('errorKeyGeneration'));
     }
+    fse.removeSync(tmpFolder);
+    this.ux.log(messages.getMessage('successCopy', [`${this.flags.env}_server.key.enc`, path.resolve(this.flags.output)]));
+    this.ux.log(messages.getMessage('successCopy', [`${this.flags.env}.crt`, path.resolve(this.flags.output)]));
     return null;
   }
 }
