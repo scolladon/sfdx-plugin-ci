e90d023cb6eaef26bacef836b1336da8babb0f9e
Sébastien Colladon <colladonsebastien@gmail.com>
2020-02-11 16:36:16 +0100

    integrate sfdx-git-delta

---
+++ messages/deltaGenerate.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "command": "Create the '%s' template file in the <output> folder",
+    "toFlag": "commit sha to where the diff is done",
+    "fromFlag": "commit sha from where the diff is done [git rev-list --max-parents=0 HEAD]",
+    "outputFlag": "source package specific output",
+    "apiVersionFlag": "salesforce API version",
+    "repoFlag": "git repository location"
+};
---
+++ src/commands/source/delta/generate.ts
@@ -0,0 +1,36 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+import * as sgd from 'sfdx-git-delta';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('sfdx-plugin-ci', 'deltaGenerate');
+
+export default class SourceDeltaGenerate extends SfdxCommand {
+
+    public static description = messages.getMessage('command', []);
+
+    protected static flagsConfig = {
+        to: flags.string({ char: 't', description: messages.getMessage('toFlag'), default: 'HEAD' }),
+        from: flags.string({ char: 'f', description: messages.getMessage('fromFlag') }),
+        output: flags.filepath({ char: 'o', description: messages.getMessage('outputFlag'), default: './output' }),
+        'api-version': flags.number({ char: 'a', description: messages.getMessage('apiVersionFlag'), default: 48.0 }),
+        repo: flags.filepath({ char: 'r', description: messages.getMessage('repoFlag'), default: '.' })
+    };
+
+    protected static requiresProject = false;
+
+    public async run(): Promise<AnyJson> {
+        return sgd({
+            'to': this.flags.to,
+            'from': this.flags.from,
+            'output': this.flags.output,
+            'apiVersion': this.flags['api-version'],
+            'repo': this.flags.repo
+        }, this.ux.log);
+    }
+}
